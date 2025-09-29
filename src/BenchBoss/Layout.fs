bf1a861de085f873dfcd3455f1fc8595e2a3f862
Rob Slavin <rob@slavin.com.au>
2025-09-29 11:40:37 +1000

    Fix svg stroke attributes

--- src/BenchBoss/Layout.fs
+++ src/BenchBoss/Layout.fs
@@ -22,13 +22,13 @@ module Layout =
       svg.children [
         Svg.path [
           svg.d "m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12"
-          svg.strokeLinecap "round"
-          svg.strokeLinejoin "round"
+          svg.strokeLineCap "round"
+          svg.strokeLineJoin "round"
         ]
         Svg.path [
           svg.d "M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
-          svg.strokeLinecap "round"
-          svg.strokeLinejoin "round"
+          svg.strokeLineCap "round"
+          svg.strokeLineJoin "round"
         ]
       ]
     ]
@@ -43,28 +43,28 @@ module Layout =
       svg.children [
         Svg.path [
           svg.d "M15 19.128a9.38 9.38 0 0 0 2.625.372 9.337 9.337 0 0 0 4.121-.952 4.125 4.125 0 0 0-7.533-2.493"
-          svg.strokeLinecap "round"
-          svg.strokeLinejoin "round"
+          svg.strokeLineCap "round"
+          svg.strokeLineJoin "round"
         ]
         Svg.path [
           svg.d "M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07"
-          svg.strokeLinecap "round"
-          svg.strokeLinejoin "round"
+          svg.strokeLineCap "round"
+          svg.strokeLineJoin "round"
         ]
         Svg.path [
           svg.d "M15 19.128v.106A12.318 12.318 0 0 1 8.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0 1 11.964-3.07"
-          svg.strokeLinecap "round"
-          svg.strokeLinejoin "round"
+          svg.strokeLineCap "round"
+          svg.strokeLineJoin "round"
         ]
         Svg.path [
           svg.d "M12 6.375a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0Z"
-          svg.strokeLinecap "round"
-          svg.strokeLinejoin "round"
+          svg.strokeLineCap "round"
+          svg.strokeLineJoin "round"
         ]
         Svg.path [
           svg.d "M20.25 8.625a2.625 2.625 0 1 1-5.25 0 2.625 2.625 0 0 1 5.25 0Z"
-          svg.strokeLinecap "round"
-          svg.strokeLinejoin "round"
+          svg.strokeLineCap "round"
+          svg.strokeLineJoin "round"
         ]
       ]
     ]
@@ -79,13 +79,13 @@ module Layout =
       svg.children [
         Svg.path [
           svg.d "M2.25 12.75V12A2.25 2.25 0 0 1 4.5 9.75h15A2.25 2.25 0 0 1 21.75 12v.75"
-          svg.strokeLinecap "round"
-          svg.strokeLinejoin "round"
+          svg.strokeLineCap "round"
+          svg.strokeLineJoin "round"
         ]
         Svg.path [
           svg.d "m13.06 6.31-2.12-2.12a1.5 1.5 0 0 0-1.061-.44H4.5A2.25 2.25 0 0 0 2.25 6v12a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18V9a2.25 2.25 0 0 0-2.25-2.25h-5.379a1.5 1.5 0 0 1-1.06-.44Z"
-          svg.strokeLinecap "round"
-          svg.strokeLinejoin "round"
+          svg.strokeLineCap "round"
+          svg.strokeLineJoin "round"
         ]
       ]
     ]
@@ -100,28 +100,28 @@ module Layout =
       svg.children [
         Svg.path [
           svg.d "M6.75 3v2.25"
-          svg.strokeLinecap "round"
-          svg.strokeLinejoin "round"
+          svg.strokeLineCap "round"
+          svg.strokeLineJoin "round"
         ]
         Svg.path [
           svg.d "M17.25 3v2.25"
-          svg.strokeLinecap "round"
-          svg.strokeLinejoin "round"
+          svg.strokeLineCap "round"
+          svg.strokeLineJoin "round"
         ]
         Svg.path [
           svg.d "M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25"
-          svg.strokeLinecap "round"
-          svg.strokeLinejoin "round"
+          svg.strokeLineCap "round"
+          svg.strokeLineJoin "round"
         ]
         Svg.path [
           svg.d "M3 18.75A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75"
-          svg.strokeLinecap "round"
-          svg.strokeLinejoin "round"
+          svg.strokeLineCap "round"
+          svg.strokeLineJoin "round"
         ]
         Svg.path [
           svg.d "M3 18.75v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5"
-          svg.strokeLinecap "round"
-          svg.strokeLinejoin "round"
+          svg.strokeLineCap "round"
+          svg.strokeLineJoin "round"
         ]
       ]
     ]
